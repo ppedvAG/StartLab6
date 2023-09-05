cf781333c8b0bd42eeac7997bc97d8286f1c1a7e
Hannes Preishuber <hannesp@ppedv.de>
2023-09-05 09:21:18 +0200

    Connection Aus Ef DBContext löschen

--- StartLab6/Models/CAspnetcoreStartlab6Startlab6AppDataKunden2MdfContext.cs
+++ StartLab6/Models/CAspnetcoreStartlab6Startlab6AppDataKunden2MdfContext.cs
@@ -19,10 +19,7 @@ public partial class CAspnetcoreStartlab6Startlab6AppDataKunden2MdfContext : DbC
 
     public virtual DbSet<Kunde> Kundes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\aspnetcore\\StartLab6\\StartLab6\\app_data\\kunden2.mdf;Integrated Security=True");
-
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Auftrag>(entity =>
